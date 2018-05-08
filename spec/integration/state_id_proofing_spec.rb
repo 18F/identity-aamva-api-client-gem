cf794741d1fff7af9cd876ce800f3f7d699c1b76
David Corwin <david.corwin@gsa.gov>
2018-05-08 14:35:29 -0700

    Update required attributes, add tests, bump version

--- spec/integration/state_id_proofing_spec.rb
+++ spec/integration/state_id_proofing_spec.rb
@@ -41,6 +41,28 @@ describe 'State ID proofing' do
         raise "Unknown result type: #{row['Result']}"
       end
     end
+
+    it "should proof for row #{row['#']} using the new api" do
+      response = Aamva::Proofer.new.proof(applicant_data(row))
+
+      if row['Result'] == 'VERIFIED'
+        expect(response.success?).to eq(true)
+      elsif row['Result'] == 'UNVERIFIED'
+        expect(response.success?).to eq(false)
+        expect(response.failed?).to eq(true)
+        error_attributes = response.errors.keys.map(&:to_s)
+        expected_error_attributes = row['Unverified Attrs'].split(',')
+        expect(Set.new(error_attributes)).to eq(Set.new(expected_error_attributes))
+      elsif row['Result'] == 'ERROR'
+        expect(response.success?).to eq(false)
+        expect(response.failed?).to eq(false)
+        expect(response.exception?).to eq(true)
+        expect(response.exception).to be_a(RuntimeError)
+        expect(response.exception.message).to include('Required attributes first_name, last_name, dob, state_id_number are not present')
+      else
+        raise "Unknown result type: #{row['Result']}"
+      end
+    end
   end
 
   def applicant_data(row)
@@ -50,13 +72,14 @@ describe 'State ID proofing' do
       last_name: row['Last Name'],
       dob: row['DOB (YYYYMMDD)'],
     }.merge(address_data(row))
+    .merge(state_id_data(row))
   end
 
   def address_data(row)
     address_elements = row['Address'].split('@')
     {
       address1: address_elements[0],
-      address2: address_elements[1],
+      # address2: address_elements[1],
       city: address_elements[2],
       state: address_elements[3],
       zipcode: address_elements[4],
