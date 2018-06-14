015186dd86691294404229ee051cfcf9e87fb6c7
Jonathan Hooper <jon9820@gmail.com>
2018-06-14 13:18:35 -0500

    Use REXML instead of ERB for user data (#17)
    **Why**: So that it escapes the data that users give us so they can't
    inject XML into our AAMVA requests

--- lib/aamva/request/verification_request.rb
+++ lib/aamva/request/verification_request.rb
@@ -1,5 +1,7 @@
 require 'erb'
 require 'httpi'
+require 'rexml/document'
+require 'rexml/xpath'
 require 'securerandom'
 
 module Aamva
@@ -11,7 +13,6 @@ module Aamva
       SOAP_ACTION = '"http://aamva.org/dldv/wsdl/2.1/IDLDVService21/VerifyDriverLicenseData"'.freeze
 
       extend Forwardable
-      def_delegators :applicant, :first_name, :last_name, :dob, :state_id_data, :address
 
       def initialize(applicant:, session_id:, auth_token:)
         @applicant = applicant
@@ -30,9 +31,18 @@ module Aamva
 
       attr_reader :applicant, :transaction_id, :auth_token
 
+      def add_user_provided_data_to_body
+        document = REXML::Document.new(body)
+        user_provided_data_map.each do |xpath, data|
+          REXML::XPath.first(document, xpath).add_text(data)
+        end
+        self.body = document.to_s
+      end
+
       def build_request_body
         renderer = ERB.new(request_body_template)
-        renderer.result(binding).gsub(/^\s+/, '').gsub(/\s+$/, '').delete("\n")
+        self.body = renderer.result(binding)
+        add_user_provided_data_to_body
       end
 
       def build_request_headers
@@ -44,13 +54,13 @@ module Aamva
       end
 
       def document_category_code
-        case state_id_data.state_id_type
+        case applicant.state_id_data.state_id_type
         when 'drivers_license'
-          1
+          '1'
         when 'drivers_permit'
-          2
+          '2'
         when 'state_id_card'
-          3
+          '3'
         end
       end
 
@@ -70,6 +80,25 @@ module Aamva
       def transaction_locator_id
         applicant.uuid
       end
+
+      # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+      def user_provided_data_map
+        applicant_address = applicant.address
+        {
+          '//ns2:IdentificationID' => applicant.state_id_data.state_id_number,
+          '//ns1:DocumentCategoryCode' => document_category_code,
+          '//ns1:MessageDestinationId' => message_destination_id,
+          '//ns2:PersonGivenName' => applicant.first_name,
+          '//ns2:PersonSurName' => applicant.last_name,
+          '//ns1:PersonBirthDate' => applicant.dob,
+          '//ns2:AddressDeliveryPointText[1]' => applicant_address.line_1,
+          '//ns2:AddressDeliveryPointText[2]' => applicant_address.line_2,
+          '//ns2:LocationCityName' => applicant_address.city,
+          '//ns2:LocationStateUsPostalServiceCode' => applicant_address.state,
+          '//ns2:LocationPostalCode' => applicant_address.zipcode,
+        }
+      end
+      # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
       def uuid
         SecureRandom.uuid
